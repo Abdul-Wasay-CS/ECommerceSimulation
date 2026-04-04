025fad00b936176dadd6d3284186c949c3bfd431
Abdul Wasay <secondarycode230651@gmail.com>
2026-04-04 13:56:38 +0500

    Refactored the color class so it returns the ANSCI escape code instead of just printing it

--- src/Color.java
+++ src/Color.java
@@ -1,34 +1,34 @@
 public final class Color {
     public static String Red() {
-        System.out.print("\033[0;31m");
+        return "\033[0;31m";
     }
 
-    public static void Green() {
-        System.out.print("\033[0;32m");
+    public static String Green() {
+        return "\033[0;32m";
     }
 
-    public static void Yellow() {
-        System.out.print("\033[0;33m");
+    public static String Yellow() {
+        return "\033[0;33m";
     }
 
-    public static void Blue() {
-        System.out.print("\033[0;34m");
+    public static String Blue() {
+        return "\033[0;34m";
     }
 
-    public static void Purple() {
-        System.out.print("\033[0;35m");
+    public static String Purple() {
+        return "\033[0;35m";
     }
 
-    public static void Cyan() {
-        System.out.print("\033[0;36m");
+    public static String Cyan() {
+        return "\033[0;36m";
     }
 
-    public static void White() {
-        System.out.print("\033[0;37m");
+    public static String White() {
+        return "\033[0;37m";
     }
 
-    public static void Reset() {
-        System.out.print("\033[0m");
+    public static String Reset() {
+        return "\033[0m";
     }
 }
 
