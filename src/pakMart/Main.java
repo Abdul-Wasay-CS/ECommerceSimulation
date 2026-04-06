3ff6e1c78c765f5a5b2c3215c5ac659d91389ffd
Abdul Wasay <secondarycode230651@gmail.com>
2026-04-06 14:58:53 +0500

    Dealt with invalid choice in main

--- src/pakMart/Main.java
+++ src/pakMart/Main.java
@@ -77,6 +77,7 @@ public class Main {
                 break;
             }
             default: {
+		System.out.print(Color.Red()+"Invalid Choice"+Color.Reset());
                 break;
             }
         }
